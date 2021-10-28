8aa1ee580269c50ef10427c7eff71bd041979653
Camila Paz <cambpaz@gmail.com>
2021-10-28 12:17:12 +0200

    hero section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,19 +8,19 @@ function NavBar () {
             <nav className="nav-bar">
                 <ul className="nav-list">
                     <li className="nav-item"><a href="#" className="nav-link">HOME</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">ABOUT US</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">BUY YOU A COFFEE</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">CREATE YOUR PLAN</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">SOBRE NOSOTROS</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">COMPRAR CAFE ROASTER</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">CONTACTO</a></li>
                     <CartWidget />
                 </ul>
             </nav>
         </header>
         <div className="hero-section">
-            <h2 className="hero-title">Great coffee made simple.</h2>
-            <p className="hero-text">Start your mornings with the world’s best coffees. 
-                Try our expertly curated artisan coffees from our best 
-                roasters delivered directly to your door, at your schedule.</p>
-            <a className="boton-hero" href="#">Create your plan</a>
+            <h2 className="hero-title">El placer de un buen café.</h2>
+            <p className="hero-text">Nada mejor mejor que un buen café para comenzar el día. 
+            Probá nuestros Roasters, un café de artesano cultivado en las cordilleras de Africa. 
+            Un café de altura.</p>
+            <a className="boton-hero" href="#">Quiero un Roaster</a>
         </div>
         </>
 )}
